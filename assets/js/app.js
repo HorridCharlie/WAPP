1139246d3a5e0e04f1ee75d63faab00357e4721d
Karol Grella <karolgrella@gmail.com>
2023-06-17 11:51:26 +0200

    added serviceWorker.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,3 +77,12 @@ let searchPosition = (positon) => {
     cityInput.value = `${positon.coords.latitude},${positon.coords.longitude}`
     weatherForm.requestSubmit()
 }
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
